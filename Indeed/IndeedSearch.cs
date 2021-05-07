1c5212f496fe75f14f0b912f7fe6d111ed949fd6
Saw Yupar Kyaw <capital-sawyuparkyaw>
2021-05-07 22:27:38 +0630

    by sawlay

--- Indeed/IndeedSearch.cs
+++ Indeed/IndeedSearch.cs
@@ -3,6 +3,7 @@
 
 
         #region Search by Key and Location on Indeed and Export to Excel         private void btnRun_Click(object sender, EventArgs e)        {            this.Cursor = Cursors.WaitCursor;            DataTable dtFinal = new DataTable();            DataTable dtResult = new DataTable();            dtResult.Columns.Add("Title");            dtResult.Columns.Add("Company");            dtResult.Columns.Add("都道府県");            dtResult.Columns.Add("Location");               dtResult.Columns.Add("お問い合わせのURL");            int j = 0;            bool go = true;//to check last page
+            bool IsLastPageNo = false;
             int start = 0;//start page = 0,next page = 10,next page = 20.......
 
             IWebDriver driver = new ChromeDriver();            do            {
@@ -24,29 +25,33 @@
                                                                                              //IList<IWebElement> arrPrefecture = driver.FindElements(By.ClassName("prefecture"));//都道府県
                 IList<IWebElement> arrLocation = driver.FindElements(By.ClassName("location"));//所在地
 
-                for (int i = 0; i < arrTitle.Count; i++)                {                    dtResult.Rows.Add();                    dtResult.Rows[j]["Title"] = arrTitle[i].Text;                    dtResult.Rows[j]["Company"] = arrCompany[i].Text;                    dtResult.Rows[j]["都道府県"] = txtLocation.Text;                    dtResult.Rows[j]["Location"] = arrLocation[i].Text;                    dtResult.Rows[j]["お問い合わせのURL"] = string.Empty;                    j++;                }                if (dtResult.Rows.Count > 0)                {                    dtFinal = dtResult.AsEnumerable()                                                     .GroupBy(x => x.Field<string>("Company"))                                                     .Select(x => x.First())                                                     .CopyToDataTable();                }                start += 50;//next page
+                for (int i = 0; i < arrTitle.Count; i++)                {                    dtResult.Rows.Add();                    dtResult.Rows[j]["Title"] = arrTitle[i].Text;                    dtResult.Rows[j]["Company"] = arrCompany[i].Text;                    dtResult.Rows[j]["都道府県"] = txtLocation.Text;                    dtResult.Rows[j]["Location"] = arrLocation[i].Text;                    dtResult.Rows[j]["お問い合わせのURL"] = string.Empty;                    j++;                }                //if (dtResult.Rows.Count > 0)                //{                //    dtFinal = dtResult.AsEnumerable()                //                                     .GroupBy(x => x.Field<string>("Company"))                //                                     .Select(x => x.First())                //                                     .CopyToDataTable();                //}                start += 50;//next page
 
-                
+
                 var CountLi = 0;
-                IList<IWebElement> arrPaging ;
+                IList<IWebElement> arrPaging;
                 try
                 {
-                    arrPaging= driver.FindElements(By.ClassName("pn"));
+                    arrPaging = driver.FindElements(By.ClassName("pn"));
                 }
-                catch {
+                catch
+                {
                     arrPaging = driver.FindElements(By.ClassName("np"));
                 }
-                CountLi = arrPaging.Count()+1;
+                if (!(arrPaging.Count() == 0))
+                {
+                    CountLi = arrPaging.Count() + 1;
 
-                var result = driver.FindElement(By.XPath("//*[@id=\"resultsCol\"]/nav/div/ul/li["+ CountLi + "]"));
-                ////*[@id="resultsCol"]/nav/div/ul/li[1]
-               // IWebElement a = driver.FindElement(By.CssSelector("ul.pagination-list li:last-child"));                bool IsLastPageNo = false;                try                {                    var bTagCheck = result.GetAttribute("innerHTML");                    if (bTagCheck.Contains("</b>"))                    {                        IsLastPageNo = true;                    }                }                catch
-                {                }
-                //IWebElement name = a.FindElement(By.TagName("b"));
-                go = false;
+                    var result = driver.FindElement(By.XPath("//*[@id=\"resultsCol\"]/nav/div/ul/li[" + CountLi + "]"));
 
-                //check next page exists
-                if (arrPaging.Count == 1)                {                    if (arrPaging[0].Text.Equals("次へ ")) { go = true; }                }                else if (arrPaging.Count == 0) go = false;                else go = true;            } while (go);            string saveFolder = @"C:\Indeed\Search_Result\2021_05_03";
+                    var bTagCheck = result.GetAttribute("innerHTML");
+                    if (bTagCheck.Contains("</b>"))
+                    {
+                        IsLastPageNo = true;
+                    }
+                }
+                else IsLastPageNo = true;
+            } while (!IsLastPageNo);            string saveFolder = @"C:\Indeed\Search_Result\2021_05_03";
             if (!Directory.Exists(saveFolder))            {                Directory.CreateDirectory(saveFolder);            }            SaveFileDialog savedialog = new SaveFileDialog();            savedialog.Filter = "Excel Files|*.xlsx;";            savedialog.Title = "Save";            savedialog.FileName = "_Result";            savedialog.InitialDirectory = saveFolder;            savedialog.RestoreDirectory = true;            if (savedialog.ShowDialog() == DialogResult.OK)            {                if (Path.GetExtension(savedialog.FileName).Contains(".xlsx"))                {                    using (XLWorkbook wb = new XLWorkbook())                    {                        wb.Worksheets.Add(dtFinal, "SearchResult");                        wb.SaveAs(savedialog.FileName);                    }                    Process.Start(Path.GetDirectoryName(savedialog.FileName));                }            }            driver.Quit();
 
             //using (XLWorkbook wb = new XLWorkbook())
@@ -76,7 +81,7 @@
 
 
         #endregion
-        #region Merge Excel files into one and Export        private void btnExportOneFile_Click(object sender, EventArgs e)        {            DataTable dt = new DataTable();            DataTable dtexcel = new DataTable();            DataTable dtexcelIntoOne = new DataTable();            string folderpath = @"C:\Indeed\Search_Result\2021_05_03";            string[] files = Directory.GetFiles(folderpath, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith("ls") || s.EndsWith("sx")).ToArray();            for (int s = 0; s < files.Length; s++)            {                string file = files[s].ToString();                Stream st = File.Open(file, FileMode.Open, FileAccess.Read);                IExcelDataReader reader = ExcelReaderFactory.CreateReader(st);                DataSet ds = reader.AsDataSet(new ExcelDataSetConfiguration()                {                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration()                    {                        UseHeaderRow = true                    }                });                dt.Merge(ds.Tables[0]);            }            dtexcel = CreateDatatable();            for (int i = 0; i < dt.Rows.Count; i++)            {                dtexcel.Rows.Add();                dtexcel.Rows[i]["Title"] = dt.Rows[i]["Title"].ToString();                dtexcel.Rows[i]["Company"] = dt.Rows[i]["Company"].ToString();                dtexcel.Rows[i]["都道府県"] = dt.Rows[i]["都道府県"].ToString();                dtexcel.Rows[i]["Location"] = dt.Rows[i]["Location"].ToString();                dtexcel.Rows[i]["お問い合わせのURL"] = dt.Rows[i]["お問い合わせのURL"].ToString();            }            if (dtexcel.Rows.Count > 0)            {                dtexcelIntoOne = dtexcel.AsEnumerable()                                                 .GroupBy(x => x.Field<string>("Company"))                                                 .Select(x => x.First())                                                 .CopyToDataTable();            }            string filePath = @"C:\Indeed";            if (!Directory.Exists(filePath))            {                Directory.CreateDirectory(filePath);            }            SaveFileDialog savedialog = new SaveFileDialog();            savedialog.Filter = "Excel Files|*.xlsx;";            savedialog.Title = "Save";            savedialog.FileName = "Result_ExcelIntoOneFile_" + System.DateTime.Now.ToString();            savedialog.InitialDirectory = filePath;            savedialog.RestoreDirectory = true;            if (savedialog.ShowDialog() == DialogResult.OK)            {                if (Path.GetExtension(savedialog.FileName).Contains(".xlsx"))                {                    using (XLWorkbook wb = new XLWorkbook())                    {                        wb.Worksheets.Add(dtexcelIntoOne, "Result");                        wb.SaveAs(savedialog.FileName);                    }                    Process.Start(Path.GetDirectoryName(savedialog.FileName));                }            }        }
+        #region Merge Excel files into one and Export        private void btnExportOneFile_Click(object sender, EventArgs e)        {            DataTable dt = new DataTable();            DataTable dtexcel = new DataTable();            DataTable dtexcelIntoOne = new DataTable();            string folderpath = @"C:\Indeed\Search_Result\2021_05_03";            string[] files = Directory.GetFiles(folderpath, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith("ls") || s.EndsWith("sx")).ToArray();            for (int s = 0; s < files.Length; s++)            {                string file = files[s].ToString();                Stream st = File.Open(file, FileMode.Open, FileAccess.Read);                IExcelDataReader reader = ExcelReaderFactory.CreateReader(st);                DataSet ds = reader.AsDataSet(new ExcelDataSetConfiguration()                {                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration()                    {                        UseHeaderRow = true                    }                });                dt.Merge(ds.Tables[0]);            }            dtexcel = CreateDatatable();            for (int i = 0; i < dt.Rows.Count; i++)            {                dtexcel.Rows.Add();                dtexcel.Rows[i]["Title"] = dt.Rows[i]["Title"].ToString();                dtexcel.Rows[i]["Company"] = dt.Rows[i]["Company"].ToString();                dtexcel.Rows[i]["都道府県"] = dt.Rows[i]["都道府県"].ToString();                dtexcel.Rows[i]["Location"] = dt.Rows[i]["Location"].ToString();                dtexcel.Rows[i]["お問い合わせのURL"] = dt.Rows[i]["お問い合わせのURL"].ToString();            }            //if (dtexcel.Rows.Count > 0)            //{            //    dtexcelIntoOne = dtexcel.AsEnumerable()            //                                     .GroupBy(x => x.Field<string>("Company"))            //                                     .Select(x => x.First())            //                                     .CopyToDataTable();            //}            string filePath = @"C:\Indeed";            if (!Directory.Exists(filePath))            {                Directory.CreateDirectory(filePath);            }            SaveFileDialog savedialog = new SaveFileDialog();            savedialog.Filter = "Excel Files|*.xlsx;";            savedialog.Title = "Save";            savedialog.FileName = "Result_ExcelIntoOneFile_" + System.DateTime.Now.ToString();            savedialog.InitialDirectory = filePath;            savedialog.RestoreDirectory = true;            if (savedialog.ShowDialog() == DialogResult.OK)            {                if (Path.GetExtension(savedialog.FileName).Contains(".xlsx"))                {                    using (XLWorkbook wb = new XLWorkbook())                    {                        wb.Worksheets.Add(dtexcelIntoOne, "Result");                        wb.SaveAs(savedialog.FileName);                    }                    Process.Start(Path.GetDirectoryName(savedialog.FileName));                }            }        }
 
 
         #endregion    }}
